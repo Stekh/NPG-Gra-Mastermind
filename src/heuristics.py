f6d3fa37ec562e70bea71ba543ca0c00350bfe8b
Stekh <bartoszstec@onet.pl>
2024-05-02 17:17:34 +0200

    add documentation to evaluate_rows()

--- src/heuristics.py
+++ src/heuristics.py
@@ -1,9 +1,16 @@
 def evaluate_row(player: list[int], secret: list[int]) -> (int, int):
+    """Evaluate given row against the given secret row
+    :param player: player's row of pins
+    :param secret: secret row of pins
+    :return: number of black and white pins, in that order
+    """
     black_pins = 0                  # type: int
     white_pins = 0                  # type: int
     mut_player = player.copy()      # type: list[int]
     mut_secret = secret.copy()      # type: list[int]
     i = 0                           # type: int
+
+    # evaluate number of black pins
     while i < len(mut_player):
         if mut_player[i] == mut_secret[i]:
             black_pins += 1
@@ -13,6 +20,7 @@ def evaluate_row(player: list[int], secret: list[int]) -> (int, int):
             i += 1
 
     i = 0
+    # evaluate number of white pins
     while i < len(mut_player):
         if mut_player[i] in mut_secret:
             white_pins += 1
