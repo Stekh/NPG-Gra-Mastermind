0abf2f9d585e12570d2bfe9b720ecf63e16c913c
Stekh <bartoszstec@onet.pl>
2024-05-02 17:09:03 +0200

    add function to evaluate row, and its test

--- src/heuristics.py
+++ src/heuristics.py
@@ -1,3 +1,22 @@
 def evaluate_row(player: list[int], secret: list[int]) -> (int, int):
+    black_pins = 0
+    white_pins = 0
+    mut_player = player.copy()
+    mut_secret = secret.copy()
+    i = 0
+    while i < len(mut_player):
+        if mut_player[i] == mut_secret[i]:
+            black_pins += 1
+            mut_player.pop(i)
+            mut_secret.pop(i)
+        else:
+            i += 1
 
-    return 0, 0
+    i = 0
+    while i < len(mut_player):
+        if mut_player[i] in mut_secret:
+            white_pins += 1
+            mut_secret.pop(mut_secret.index(mut_player[i]))
+            mut_player.pop(i)
+
+    return black_pins, white_pins
--- test/test_heuristics.py
+++ test/test_heuristics.py
@@ -3,4 +3,6 @@ from src import heuristics
 
 
 def test_evaluate_row():
-    assert heuristics.evaluate_row([1, 2, 3, 4], [1, 2, 3, 4]) == (0, 0)
+    assert heuristics.evaluate_row([1, 2, 3, 4], [1, 2, 3, 4]) == (4, 0)
+    assert heuristics.evaluate_row([1, 2, 3, 4], [4, 3, 2, 1]) == (0, 4)
+    assert heuristics.evaluate_row([1, 2, 3, 4], [4, 2, 3, 1]) == (2, 2)
