832c9eb70d73f7a13d2211f85544772d69491f85
Stekh <bartoszstec@onet.pl>
2024-05-02 17:12:58 +0200

    add comment type hints for variables

--- src/heuristics.py
+++ src/heuristics.py
@@ -1,9 +1,9 @@
 def evaluate_row(player: list[int], secret: list[int]) -> (int, int):
-    black_pins = 0
-    white_pins = 0
-    mut_player = player.copy()
-    mut_secret = secret.copy()
-    i = 0
+    black_pins = 0                  # type: int
+    white_pins = 0                  # type: int
+    mut_player = player.copy()      # type: list[int]
+    mut_secret = secret.copy()      # type: list[int]
+    i = 0                           # type: int
     while i < len(mut_player):
         if mut_player[i] == mut_secret[i]:
             black_pins += 1
