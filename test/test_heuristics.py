ec1ef9e79ae97b5539ea82f10cd2086938c576e4
Stekh <bartoszstec@onet.pl>
2024-05-02 17:36:01 +0200

    add one final test case

--- test/test_heuristics.py
+++ test/test_heuristics.py
@@ -7,3 +7,4 @@ def test_evaluate_row():
     assert heuristics.evaluate_row([1, 2, 3, 4], [4, 3, 2, 1]) == (0, 4)
     assert heuristics.evaluate_row([1, 2, 3, 4], [4, 2, 3, 1]) == (2, 2)
     assert heuristics.evaluate_row([1, 2, 3, 4], [5, 6, 7, 8]) == (0, 0)
+    assert heuristics.evaluate_row([1, 2, 3, 4], [1, 2, 4, 1]) == (2, 1)
