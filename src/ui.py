04e105d3850100275a53b32051da699c13c69112
PS <solarz.piotr1@gmail.com>
2024-05-04 20:20:15 +0200

    correct code to match CSG

--- src/ui.py
+++ src/ui.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import void
 
 BIG_BUTTON_WIDTH = 32
 BIG_BUTTON_HEIGHT = 32
@@ -7,54 +8,64 @@ SMALL_BUTTON_HEIGHT = 16
 
 
 class button:
-    def __init__(self, x, y, width, height, colors, hover_color, click_count=0, hover=False):
+    """Button is a singular object that represents an interactable button.
+    Attributes: coordinates, size, hover and click colors
+    Methods allow for drawing, updating and interpreting click count and hover state"""
+
+    def __init__(self, x: float, y: float, width: float, height: float, colors: tuple, hover_color: tuple, click_count=0, hover=False):
         self.click_count = click_count
         self.hover = hover
         self.hover_color = hover_color
         self.colors = colors
         self.rect = pg.Rect(x, y, width, height)
 
-    def draw(self, screen):
+    def draw(self, screen) -> None:
+        """Puts the button on the screen in its place"""
         color = self.colors[self.click_count % len(self.colors)]
         if self.hover:
             color = self.hover_color
 
         pg.draw.rect(screen, color, self.rect)
 
-    def is_mouse_over(self, pos):
+    def is_mouse_over(self, pos) -> bool:
+        """Checks for mouse hover position relative to the button"""
         return self.rect.collidepoint(pos)
 
-    def set_hover(self, hover):
+    def set_hover(self, hover) -> None:
+        """Updates the mouse hover status"""
         self.hover = hover
 
-    def next_click(self):
+    def next_click(self) -> None:
+        """Increments the amount of clicks on the button"""
         self.click_count += 1
 
 
-buttons = []
+buttons = []  # array of all buttons to display on the screen
 for j in range(1, 5):
     for i in range(1, 11):
+        # p1 buttons
         x1 = 16 + 48 * i
         y1 = 48 * j
         buttons.append(
-            button(x1, y1, BIG_BUTTON_WIDTH, BIG_BUTTON_HEIGHT, [(255, 255, 255), (255, 0, 0), (255, 255, 0), (0, 255, 0), (127, 0, 255) ], (127, 127, 127)))
+            button(x1, y1, BIG_BUTTON_WIDTH, BIG_BUTTON_HEIGHT,
+                   [(255, 255, 255), (255, 0, 0), (255, 255, 0), (0, 255, 0), (127, 0, 255)], (127, 127, 127)))
 
+        # p2 buttons
         x2 = x1 + 8
         y2 = 260 + 24 * j
         buttons.append(
-            button(x2,y2, SMALL_BUTTON_WIDTH, SMALL_BUTTON_HEIGHT, [(64, 64, 64), (0, 64, 64), (255, 255, 255)], (127, 127, 127))
-        )
+            button(x2, y2, SMALL_BUTTON_WIDTH, SMALL_BUTTON_HEIGHT, [(64, 64, 64), (0, 64, 64), (255, 255, 255)],
+                   (127, 127, 127)))
 
 
-
-
-
-
-def ui(screen, mouse_state):
+def ui(screen, mouse_state) -> None:
+    """Updates UI based on mouse position"""
     for b in buttons:
+
         pos = mouse_state[1]
         is_mouse_over = b.is_mouse_over(pos)
         b.set_hover(is_mouse_over)
+
         if is_mouse_over:
             clicked = mouse_state[0]
             if clicked:
@@ -63,5 +74,3 @@ def ui(screen, mouse_state):
         b.draw(screen)
 
     pg.display.flip()
-
-# pg.draw.rect(screen, (10 * i, 10 * i + 5 * j, 10 * i), pg.Rect(16 + 48 * i, 48 * j, 32, 32))
