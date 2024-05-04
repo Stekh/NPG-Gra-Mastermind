4afa6e027b9ad377aef5d76cbca960f4176aea3e
Stekh <bartoszstec@onet.pl>
2024-05-04 12:32:37 +0200

    add test for assign_points()

--- test/test_heuristics.py
+++ test/test_heuristics.py
@@ -16,3 +16,8 @@ class TestHeuristics:
         assert heuristics.evaluate_round([1, 2, 3, 4], [4, 2, 3, 1]) == (False, 2, 2)
         assert heuristics.evaluate_round([1, 2, 3, 4], [5, 6, 7, 8]) == (False, 0, 0)
         assert heuristics.evaluate_round([1, 2, 3, 4], [1, 2, 4, 1]) == (False, 2, 1)
+
+    def test_assign_points(self):
+        assert heuristics.assign_points([1, 2, 3, 4], [1, 2, 3, 4], 3, 10) == (0, 4, 0)
+        assert heuristics.evaluate_round([1, 2, 3, 4], [4, 3, 2, 1], 10, 10) == (2, 0, 4)
+        assert heuristics.evaluate_round([1, 2, 3, 4], [4, 2, 3, 1], 7, 10) == (1, 2, 2)
