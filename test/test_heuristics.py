20adfa00292700be56dac4814dd015867d3fea58
Stekh <bartoszstec@onet.pl>
2024-05-04 12:18:23 +0200

    fix minor typo in one test case

--- test/test_heuristics.py
+++ test/test_heuristics.py
@@ -15,4 +15,4 @@ def test_evaluate_round():
     assert heuristics.evaluate_round([1, 2, 3, 4], [4, 3, 2, 1]) == (False, 0, 4)
     assert heuristics.evaluate_round([1, 2, 3, 4], [4, 2, 3, 1]) == (False, 2, 2)
     assert heuristics.evaluate_round([1, 2, 3, 4], [5, 6, 7, 8]) == (False, 0, 0)
-    assert heuristics.evaluate_round([1, 2, 3, 4], [1, 2, 4, 1]) == (False, 2, 2)
+    assert heuristics.evaluate_round([1, 2, 3, 4], [1, 2, 4, 1]) == (False, 2, 1)
