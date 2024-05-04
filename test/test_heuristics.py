8c1f263b981e5bd3f6d427878b082868a8b50697
Stekh <bartoszstec@onet.pl>
2024-05-04 12:21:05 +0200

    wrap all tests in a class, to make running them easier

--- test/test_heuristics.py
+++ test/test_heuristics.py
@@ -2,17 +2,17 @@ import pytest
 from src import heuristics
 
 
-def test_evaluate_row():
-    assert heuristics.evaluate_row([1, 2, 3, 4], [1, 2, 3, 4]) == (4, 0)
-    assert heuristics.evaluate_row([1, 2, 3, 4], [4, 3, 2, 1]) == (0, 4)
-    assert heuristics.evaluate_row([1, 2, 3, 4], [4, 2, 3, 1]) == (2, 2)
-    assert heuristics.evaluate_row([1, 2, 3, 4], [5, 6, 7, 8]) == (0, 0)
-    assert heuristics.evaluate_row([1, 2, 3, 4], [1, 2, 4, 1]) == (2, 1)
+class TestHeuristics:
+    def test_evaluate_row(self):
+        assert heuristics.evaluate_row([1, 2, 3, 4], [1, 2, 3, 4]) == (4, 0)
+        assert heuristics.evaluate_row([1, 2, 3, 4], [4, 3, 2, 1]) == (0, 4)
+        assert heuristics.evaluate_row([1, 2, 3, 4], [4, 2, 3, 1]) == (2, 2)
+        assert heuristics.evaluate_row([1, 2, 3, 4], [5, 6, 7, 8]) == (0, 0)
+        assert heuristics.evaluate_row([1, 2, 3, 4], [1, 2, 4, 1]) == (2, 1)
 
-
-def test_evaluate_round():
-    assert heuristics.evaluate_round([1, 2, 3, 4], [1, 2, 3, 4]) == (True, 4, 0)
-    assert heuristics.evaluate_round([1, 2, 3, 4], [4, 3, 2, 1]) == (False, 0, 4)
-    assert heuristics.evaluate_round([1, 2, 3, 4], [4, 2, 3, 1]) == (False, 2, 2)
-    assert heuristics.evaluate_round([1, 2, 3, 4], [5, 6, 7, 8]) == (False, 0, 0)
-    assert heuristics.evaluate_round([1, 2, 3, 4], [1, 2, 4, 1]) == (False, 2, 1)
+    def test_evaluate_round(self):
+        assert heuristics.evaluate_round([1, 2, 3, 4], [1, 2, 3, 4]) == (True, 4, 0)
+        assert heuristics.evaluate_round([1, 2, 3, 4], [4, 3, 2, 1]) == (False, 0, 4)
+        assert heuristics.evaluate_round([1, 2, 3, 4], [4, 2, 3, 1]) == (False, 2, 2)
+        assert heuristics.evaluate_round([1, 2, 3, 4], [5, 6, 7, 8]) == (False, 0, 0)
+        assert heuristics.evaluate_round([1, 2, 3, 4], [1, 2, 4, 1]) == (False, 2, 1)
