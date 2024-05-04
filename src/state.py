dab34dd6bf98211ca53b4d9d621b4d4defd5a93d
Jan Sosnowiec <jsosnowiec@student.agh.edu.pl>
2024-05-04 12:24:43 +0200

    Added moving the active row and appropriate tests

--- src/state.py
+++ src/state.py
@@ -1,19 +1,20 @@
 class Game:
     """The core game state class.
 
-    Holds two 2D matrices of pins currently on the board:
-        color_pin_matrix -- colored pins used for guessing
-        response_pin_matrix -- black and white pins used for verifying guesses
-    Initialise with the size of the game and the target combination"""
+            Holds two 2D matrices of pins currently on the board:
+                color_pin_matrix -- colored pins used for guessing
+                response_pin_matrix -- black and white pins used for verifying guesses
+            Initialise with the size of the game and the target combination"""
 
     def __init__(self, no_rows: int, no_cols: int, combination: list[int]):
+
         self.__active_row = 0
         self.__no_rows = no_rows
         self.__no_cols = no_cols
         self.__combination = combination
         self.__color_pin_matrix = [[0 for i in range(no_cols)] for j in range(no_rows)]
         self.__response_pin_matrix = [[0 for i in range(no_cols)] for j in range(no_rows)]
-        self.combination_found = False
+        self.end_row_reached = False
 
     def get_color_pin_color(self, row: int, col: int):
         """Color pin getter."""
@@ -47,3 +48,13 @@ class Game:
         """Places a response pin in given position of active row."""
         self.__response_pin_matrix[self.__active_row][pos] = color
 
+    def advance_active_row(self):
+        """Moves game to the next row, or records end of game if the last row is reached."""
+        if self.__active_row < self.__no_rows - 1:
+            self.__active_row += 1
+        else:
+            self.end_row_reached = True
+
+    def set_active_row(self, pos: int):  # Probably won't need this but will help with tests
+        """Sets the active row to a specified location."""
+        self.__active_row = pos
--- test/test_state.py
+++ test/test_state.py
@@ -12,3 +12,24 @@ def test_getters():
     assert game.get_active_row() == 0
     assert game.get_color_pin_color(0, 2) == 3
     assert game.get_response_pin_color(0, 3) == 2
+
+
+def test_active_row_movement():
+    game = state.Game(10, 4, [2, 2, 3, 4])
+    game.place_color_pin(2, 3)
+    game.place_response_pin(1, 2)
+    game.advance_active_row()
+    game.place_color_pin(1, 3)
+    game.place_response_pin(2, 2)
+    assert game.get_color_pin_color(0, 3) == 2
+    assert game.get_response_pin_color(0, 2) == 1
+    assert game.get_color_pin_color(1, 3) == 1
+    assert game.get_response_pin_color(1, 2) == 2
+
+
+def test_end_of_space():
+    game = state.Game(10, 4, [5, 6, 7, 3])
+    game.set_active_row(9)
+    game.advance_active_row()
+    assert game.get_active_row() == 9
+    assert game.end_row_reached
