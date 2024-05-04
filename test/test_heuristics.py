109c4fa16bed0f87198cf1576c53c2987e17e4d4
Stekh <bartoszstec@onet.pl>
2024-05-04 12:11:26 +0200

    Add first test case for evaluate_round()

--- test/test_heuristics.py
+++ test/test_heuristics.py
@@ -8,3 +8,7 @@ def test_evaluate_row():
     assert heuristics.evaluate_row([1, 2, 3, 4], [4, 2, 3, 1]) == (2, 2)
     assert heuristics.evaluate_row([1, 2, 3, 4], [5, 6, 7, 8]) == (0, 0)
     assert heuristics.evaluate_row([1, 2, 3, 4], [1, 2, 4, 1]) == (2, 1)
+
+
+def test_evaluate_round():
+    assert heuristics.evaluate_round([1, 2, 3, 4], [1, 2, 3, 4]) == (True, 0, 4, 0)
