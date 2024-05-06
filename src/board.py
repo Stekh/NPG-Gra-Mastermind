b17d6add20199f877e3240509b47f19d561b2981
Franciszek Slusarczyk <frslusarczyk@student.agh.edu.pl>
2024-05-06 19:07:43 +0200

    extended board class functionality

--- src/board.py
+++ src/board.py
@@ -16,3 +16,8 @@ class Board:
         self.board_color = board_color
         self.pin_color = pin_color
         self.rect = pg.Rect(x, y, CELL_WIDTH*cols, CELL_HEIGHT*rows)
+        init_pos_x: float
+        init_pos_y: float
+        init_pos_x = x + (CELL_WIDTH - HOLE_WIDTH)/2
+        init_pos_y = y + (CELL_HEIGHT - HOLE_HEIGHT)/2
+        self.holes=[[ui.Button(init_pos_x + i*CELL_WIDTH,init_pos_y + j*CELL_HEIGHT,HOLE_WIDTH,HOLE_HEIGHT,[(120, 120, 120)],(120, 0, 120)) for i in range(0,cols)] for j in range(0,rows)]
